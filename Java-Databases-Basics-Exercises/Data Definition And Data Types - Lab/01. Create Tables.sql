76d1d010a7c1e2f7d83a5216d8d41a5429d84433
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-26 15:37:34 +0300

    Update 01. Create Tables.sql

--- Java-Databases-Basics-Exercises/Data Definition And Data Types - Lab/01. Create Tables.sql
+++ Java-Databases-Basics-Exercises/Data Definition And Data Types - Lab/01. Create Tables.sql
@@ -12,3 +12,22 @@ CREATE TABLE `products` (
 	`name` VARCHAR(50) NOT NULL,
     category_id INT NOT NULL
     );
+
+
+# with foreign key on the last column:
+
+CREATE TABLE `employees` (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+    );
+CREATE TABLE `categories` (
+	id INT primary key NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL
+    );
+CREATE TABLE `products` (
+	id INT primary key NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL,
+    category_id INT, 
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+    );
