9a1f7de203f927ac68f07b159114b0d39cab9b12
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-26 16:07:33 +0300

    Update 04. Adding Constraints.sql

--- Java-Databases-Basics-Exercises/Data Definition And Data Types - Lab/04. Adding Constraints.sql
+++ Java-Databases-Basics-Exercises/Data Definition And Data Types - Lab/04. Adding Constraints.sql
@@ -6,3 +6,9 @@ FOREIGN KEY (category_id) REFERENCES categories(id);
 -- Step 2: Allow NULL values
 ALTER TABLE products
 MODIFY COLUMN category_id INT NULL;
+
+-- The first statement adds a foreign key constraint to the category_id column in the products table, referencing the id column in the categories table.
+
+-- The second statement modifies the category_id column to allow NULL values, which is necessary because foreign key constraints cannot be added to columns that are --defined as NOT NULL.
+
+-- Note that if the category_id column already contains data that violates the foreign key constraint, the ALTER TABLE statement will fail. You will need to update the data in the category_id column to match the values in the referenced column in the categories table before adding the foreign key constraint.
