24457d4d54ddfd85d7a7f52262709303fe6e0965
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-26 15:35:11 +0300

    Create 01. Create Tables.sql

---
+++ Java-Databases-Basics-Exercises/Data Definition And Data Types - Lab/01. Create Tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `employees` (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+    );
+CREATE TABLE `categories` (
+	id INT primary key NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL
+    );
+CREATE TABLE `products` (
+	id INT primary key NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL,
+    category_id INT NOT NULL
+    );
