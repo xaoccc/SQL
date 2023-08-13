8e5ad5d6ef8251ec0b9c7c1b6cdd556a59928bba
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-13 21:53:09 +0300

    it is the same

--- Java-Databases-Basics-Exercises/Data Definition and Data Types - Exercise/13. Basic Insert.sql
+++ Java-Databases-Basics-Exercises/Data Definition and Data Types - Exercise/13. Basic Insert.sql
@@ -48,14 +48,7 @@ VALUES ("Engineering"),
 	("Software Development"),
     ("Quality Assurance");    
     
-<<<<<<< HEAD
-INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`, `address_id`)
-VALUES ("Ivan", "Ivanov", "Ivanov", ".NET Developer", 4, "2013-02-01", "3500.00", 1), 
-	("Petar", "Petrov", "Petrov", "Senior Engineer", 1, "2004-03-02", "4000.00", 2), 
-	("Maria", "Petrova", "Ivanova ", "Intern", 5, "2016-08-28", "525.25", 3), 
-	("Georgi", "Terziev", "Ivanov", "CEO", 2, "2007-12-09", "3000.00", 4), 
-    ("Peter", "Pan", "Pan", "Intern", 3, "2016-08-28", "599.88", 5); 
-=======
+
 INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
 VALUES ("Ivan", "Ivanov", "Ivanov", ".NET Developer", 4, "2013-02-01", "3500.00"), 
 	("Petar", "Petrov", "Petrov", "Senior Engineer", 1, "2004-03-02", "4000.00"), 
