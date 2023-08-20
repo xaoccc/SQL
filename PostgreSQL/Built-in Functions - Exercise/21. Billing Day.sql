df5e7cdd1fb9c192c1b1e56297747cc5a81972d3
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-20 22:11:31 +0300

    Update 21. Billing Day.sql

--- PostgreSQL/Built-in Functions - Exercise/21. Billing Day.sql
+++ PostgreSQL/Built-in Functions - Exercise/21. Billing Day.sql
@@ -1,2 +1,4 @@
 ALTER TABLE "bookings"
-ADD "billing_day" TIMESTAMPTZ;
+ADD COLUMN "billing_day" TIMESTAMPTZ default CURRENT_TIMESTAMP;
+
+SELECT to_char("billing_day", 'DD "Day" MM "Month" YYYY "Year" HH24:MI:SS')  AS "Billing Day" FROM "bookings";
