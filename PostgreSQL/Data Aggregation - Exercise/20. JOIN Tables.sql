6f6fdbe41dfbc67cd30c4f0f856d9500f95da20d
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-24 20:36:04 +0300

    666

--- PostgreSQL/Data Aggregation - Exercise/20. JOIN Tables.sql
+++ PostgreSQL/Data Aggregation - Exercise/20. JOIN Tables.sql
@@ -1,3 +1,2 @@
-SELECT departments.*, employees.*
-FROM departments 
-FULL JOIN employees ON employees.department_id = departments.id;
+SELECT * FROM departments 
+JOIN employees ON employees.department_id = departments.id;
