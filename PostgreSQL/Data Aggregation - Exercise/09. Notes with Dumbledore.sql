fe2c8b998c6c90e60bd71d48f9d9b58c2d2bec87
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-25 17:21:06 +0300

    Update 09. Notes with Dumbledore.sql

--- PostgreSQL/Data Aggregation - Exercise/09. Notes with Dumbledore.sql
+++ PostgreSQL/Data Aggregation - Exercise/09. Notes with Dumbledore.sql
@@ -1,4 +1,3 @@
 SELECT "last_name" , COUNT("notes") AS "Notes with Dumbledore"  FROM wizard_deposits
 WHERE "notes" LIKE '%Dumbledore%'
-GROUP BY "last_name"
-ORDER BY "Notes with Dumbledore";
+GROUP BY "last_name";
