b0b6e34e734c6703edb52a8cd88757267c563df1
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-25 17:28:33 +0300

    Update 10. Wizard View.sql

--- PostgreSQL/Data Aggregation - Exercise/10. Wizard View.sql
+++ PostgreSQL/Data Aggregation - Exercise/10. Wizard View.sql
@@ -1,7 +1,7 @@
-CREATE OR REPLACE VIEW "view_wizzard_deposits_with_expiration_date_before_1983_08_17" AS 
+CREATE OR REPLACE VIEW "view_wizard_deposits_with_expiration_date_before_1983_08_17" AS 
 SELECT CONCAT("first_name", ' ', "last_name") AS "Wizard Name", "deposit_start_date" AS "Start Date", "deposit_expiration_date" AS "Expiration Date", "deposit_amount"  AS "Amount" FROM "wizard_deposits";
 
-SELECT * FROM "view_wizzard_deposits_with_expiration_date_before_1983_08_17"
+SELECT * FROM "view_wizard_deposits_with_expiration_date_before_1983_08_17"
 WHERE "Expiration Date" <= '1983-08-17'
 GROUP BY "Wizard Name", "Start Date", "Expiration Date", "Amount"
 ORDER BY "Expiration Date" ASC;
