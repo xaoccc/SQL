dd1d76c3f41c019c785b62d6e1aa0c8fd0771504
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-28 10:24:10 +0300

    Update 03. Customs.sql

--- PostgreSQL/Table Relations - Exercise/03. Customs.sql
+++ PostgreSQL/Table Relations - Exercise/03. Customs.sql
@@ -12,13 +12,13 @@ CREATE TABLE "people" (
 	"id" SERIAL PRIMARY KEY,
 	"first_name" VARCHAR(50),
 	"salary" NUMERIC(10,2),
-	"passport_id"  INT,
+	"passport_id" INT NOT NULL UNIQUE,
 	CONSTRAINT "people_passport_id"
 	FOREIGN KEY (passport_id)
 	REFERENCES passports(id)
 );
 
-INSERT INTO "people" ("first_name", "salary", "passport_id")
+INSERT INTO people (first_name, salary, passport_id)
 		VALUES ('Roberto', 43300.0000, 101),
 	('Tom', 56100.0000, 102),
 	('Yana', 60200.0000, 100);
