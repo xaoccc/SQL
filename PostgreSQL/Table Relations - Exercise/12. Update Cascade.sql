f1ed4d9400d093ad776d232e4929d3bc7d82e27a
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-29 14:38:50 +0300

    Update 12. Update Cascade.sql

--- PostgreSQL/Table Relations - Exercise/12. Update Cascade.sql
+++ PostgreSQL/Table Relations - Exercise/12. Update Cascade.sql
@@ -1,9 +1,9 @@
 ALTER TABLE "countries_rivers"
 ADD CONSTRAINT fk_countries_rivers_rivers
 FOREIGN KEY (river_id)
-REFERENCES rivers(id) ON DELETE CASCADE;
+REFERENCES rivers(id) ON UPDATE CASCADE;
 
 ALTER TABLE "countries_rivers"
 ADD CONSTRAINT fk_countries_rivers_countries
 FOREIGN KEY (country_code)
-REFERENCES countries(country_code) ON DELETE CASCADE;
+REFERENCES countries(country_code) ON UPDATE CASCADE;
