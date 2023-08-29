9ebff732c6388e98fc11140527ea6a982151745f
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-29 12:06:48 +0300

    Create 10. Online Store.sql

---
+++ PostgreSQL/Table Relations - Exercise/10. Online Store.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS "cities" (
+	"id" SERIAL PRIMARY KEY,
+	"city_name" VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS "customers" (
+	"id" SERIAL PRIMARY KEY,
+	"customer_name" VARCHAR(50),
+	"birthday" DATE,
+	"city_id" INT NOT NULL,
+	CONSTRAINT fk_customers_cities
+	FOREIGN KEY (city_id)
+	REFERENCES cities(id)	
+);
+
+CREATE TABLE IF NOT EXISTS "item_types" (
+	"id" SERIAL PRIMARY KEY,
+	"item_type_name" VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS "items" (
+	"id" SERIAL PRIMARY KEY,
+	"item_name" VARCHAR(50),
+	"item_type_id" INT NOT NULL,
+	CONSTRAINT fk_items_item_types
+	FOREIGN KEY (item_type_id)
+	REFERENCES item_types(id)	
+);
+
+CREATE TABLE IF NOT EXISTS "orders" (
+	"id" SERIAL PRIMARY KEY,
+	"customer_id" INT NOT NULL,
+	CONSTRAINT fk_orders_customers
+	FOREIGN KEY (customer_id)
+	REFERENCES customers(id)	
+);
+
+CREATE TABLE IF NOT EXISTS "order_items" (
+	"order_id" SERIAL PRIMARY KEY,
+	"item_id" INT NOT NULL,
+	CONSTRAINT fk_order_items_items
+	FOREIGN KEY (item_id)
+	REFERENCES items(id),
+	
+	CONSTRAINT fk_order_items_orders
+	FOREIGN KEY (order_id)
+	REFERENCES orders(id)
+);
+
+
+
+
+
+
