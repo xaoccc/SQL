d78da5fe2642b93e331b46e746367b262c70269a
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-29 14:49:21 +0300

    Create 13. SET NULL.sql

---
+++ PostgreSQL/Table Relations - Exercise/13. SET NULL.sql
@@ -0,0 +1,13 @@
+-- Database: table_relations_geography_db
+
+-- DROP DATABASE IF EXISTS table_relations_geography_db;
+
+CREATE DATABASE table_relations_geography_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Bulgarian_Bulgaria.1252'
+    LC_CTYPE = 'Bulgarian_Bulgaria.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
