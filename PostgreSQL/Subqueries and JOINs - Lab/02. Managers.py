cabf5d93143b6e4f05c6a98ccf2b3ff916730d72
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-08-30 13:36:25 +0300

    Update 02. Managers.py

--- PostgreSQL/Subqueries and JOINs - Lab/02. Managers.py
+++ PostgreSQL/Subqueries and JOINs - Lab/02. Managers.py
@@ -1,4 +1,4 @@
 SELECT employees.employee_id, CONCAT(first_name, ' ', last_name) AS full_name, departments.department_id, name FROM  departments
-JOIN employees ON departments.department_id = employees.department_id
+JOIN employees ON departments.manager_id = employees.employee_id
 ORDER BY employees.employee_id
 LIMIT 5;
