d77736fed300d6eff6eea650b5b5aff02e3d69aa
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-04 16:38:20 +0300

    Delete 02. Managers.py

--- PostgreSQL/Subqueries and JOINs - Lab/02. Managers.py
+++
@@ -1,4 +0,0 @@
-SELECT employees.employee_id, CONCAT(first_name, ' ', last_name) AS full_name, departments.department_id, name FROM  departments
-JOIN employees ON departments.manager_id = employees.employee_id
-ORDER BY employees.employee_id
-LIMIT 5;
