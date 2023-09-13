00f1b6fdbe449bb28a766a597154a1f895ab1fe7
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-13 11:17:54 +0300

    Update 01. Create Tables.sql

--- PostgreSQL/Data Types and Table Basics - Lab/01. Create Tables.sql
+++ PostgreSQL/Data Types and Table Basics - Lab/01. Create Tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE employees (
 	id serial PRIMARY KEY NOT NULL,
 	first_name VARCHAR(30),
   	last_name VARCHAR(50),
-  	hiring_date DATE,
+  	hiring_date DATE NOT NULL DEFAULT '2023-01-01',
   	salary NUMERIC(10,2),
   	devices_number INTEGER
 );
