16f156faea8341b3a0e894dc3d8ebd88c7f85b76
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-13 13:40:00 +0300

    Update 18. Arithmetical Operators.sql

--- PostgreSQL/Built-in Functions - Exercise/18. Arithmetical Operators.sql
+++ PostgreSQL/Built-in Functions - Exercise/18. Arithmetical Operators.sql
@@ -1,8 +1,11 @@
 CREATE TABLE "bookings_calculation" AS
-	SELECT 
-		"booked_for", "booked_for" * 50 AS "multiplication", "booked_for" % 50 AS "modulo" 
-		
-	FROM  "bookings"
+	SELECT "booked_for" FROM  "bookings"
 	WHERE "apartment_id" = 93;
-		
-	
+
+ALTER TABLE "bookings_calculation"
+	ADD COLUMN "multiplication" NUMERIC,
+	ADD COLUMN "modulo" NUMERIC;
+
+UPDATE "bookings_calculation"
+	SET "multiplication" = "booked_for" * 50,
+    	"modulo" = "booked_for" % 50;
