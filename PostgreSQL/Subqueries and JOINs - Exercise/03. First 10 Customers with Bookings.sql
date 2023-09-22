9dfafbfb136b04cb2c8c80a3228ea834be0fe849
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-22 09:36:05 +0300

    Update 03. First 10 Customers with Bookings.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/03. First 10 Customers with Bookings.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/03. First 10 Customers with Bookings.sql
@@ -1,4 +1,7 @@
-SELECT "booking_id" AS "Booking ID", DATE("starts_at") AS "Start Date", "apartment_id" AS "Apartment ID", CONCAT("customers"."first_name", ' ', "customers"."last_name" ) AS "Customer Name" FROM "bookings"
+SELECT "booking_id" AS "Booking ID", 
+  DATE("starts_at") AS "Start Date", 
+  "apartment_id" AS "Apartment ID", 
+  CONCAT("customers"."first_name", ' ', "customers"."last_name" ) AS "Customer Name" FROM "bookings"
 RIGHT JOIN "customers" ON "bookings"."customer_id" = "customers"."customer_id"
 ORDER BY "Customer Name" 
 LIMIT 10;
