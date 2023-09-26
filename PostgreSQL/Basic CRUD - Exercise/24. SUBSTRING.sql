48af018db9f37e00721531aea8547ab0982ff5f7
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-26 20:44:52 +0300

    Update 24. SUBSTRING.sql

--- PostgreSQL/Basic CRUD - Exercise/24. SUBSTRING.sql
+++ PostgreSQL/Basic CRUD - Exercise/24. SUBSTRING.sql
@@ -1,12 +1,9 @@
 CREATE VIEW "view_initials" AS
-	SELECT "first_name", "last_name" FROM "employees"
+	SELECT SUBSTRING("first_name", 1, 2) AS initial, "last_name" FROM "employees"
 	ORDER BY last_name;
+
 	
-UPDATE "view_initials"
-	SET "first_name" = LEFT("first_name", 2);
-	
-ALTER VIEW "view_initials"
-	RENAME COLUMN "first_name" TO "initial";
+
 	
 
 
