220e3dae9000afcea62f6dce60bcde8d9340acd3
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-26 17:04:14 +0300

    Update 17. Award Employees with Experience.sql

--- PostgreSQL/Basic CRUD - Exercise/17. Award Employees with Experience.sql
+++ PostgreSQL/Basic CRUD - Exercise/17. Award Employees with Experience.sql
@@ -1,4 +1,3 @@
 UPDATE employees
 SET "job_title" = CONCAT('Senior ', "job_title"), "salary" = "salary" + 1500
-WHERE hire_date > '1998-01-01 00:00:00' AND  hire_date < '2000-01-06 00:00:00';
-
+WHERE hire_date BETWEEN  '1998-01-01' AND '2000-01-06';
