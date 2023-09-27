4558cf8802d5c4645fc3a08f9881c00af17455be
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-27 22:02:00 +0300

    Update 22. EXTRACT Booked At.sql

--- PostgreSQL/Built-in Functions - Exercise/22. EXTRACT Booked At.sql
+++ PostgreSQL/Built-in Functions - Exercise/22. EXTRACT Booked At.sql
@@ -1,7 +1,7 @@
 SELECT to_char("booked_at", 'YYYY') AS "YEAR", 
-to_char("booked_at" AT TIME ZONE 'UTC', 'FMMM') AS "MONTH" , 
-to_char("booked_at" AT TIME ZONE 'UTC', 'FMDD') AS "DAY", 
+to_char("booked_at", 'FMMM') AS "MONTH" ,
+to_char("booked_at", 'FMDD') AS "DAY",
 to_char("booked_at" AT TIME ZONE 'UTC', 'FMHH24') AS "HOUR", 
-to_char("booked_at" AT TIME ZONE 'UTC', 'FMMI') AS "MINUTE",
+to_char("booked_at", 'FMMI') AS "MINUTE",
 CEILING(EXTRACT(SECOND FROM "booked_at" AT TIME ZONE 'UTC')) AS "SECOND"
   FROM "bookings";
