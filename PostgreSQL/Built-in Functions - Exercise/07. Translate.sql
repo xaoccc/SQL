9f9a7db413f9a65d73e8e9385f73b6bda2ec6085
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-27 11:19:09 +0300

    Update 07. Translate.sql

--- PostgreSQL/Built-in Functions - Exercise/07. Translate.sql
+++ PostgreSQL/Built-in Functions - Exercise/07. Translate.sql
@@ -1,3 +1,1 @@
-CREATE VIEW "translate" AS 
-	SELECT "capital", TRANSLATE("capital", 'áãåçéíñóú' , 'aaaceinou' ) AS translated_name FROM "countries";	
-SELECT * FROM "translate";
+SELECT "capital", TRANSLATE("capital", 'áãåçéíñóú' , 'aaaceinou' ) AS translated_name FROM "countries";	
