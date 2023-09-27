9783722f80129a0ce5259fd88d6a760f9753c9d2
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-27 11:16:58 +0300

    Update 05. Substring River Length.sql

--- PostgreSQL/Built-in Functions - Exercise/05. Substring River Length.sql
+++ PostgreSQL/Built-in Functions - Exercise/05. Substring River Length.sql
@@ -1,3 +1,1 @@
-CREATE VIEW "river_length" AS
-	SELECT SUBSTRING("River Information" , '([0-9]{1,4})' ) FROM "view_river_info" ;	
-SELECT * FROM "river_length";
+SELECT SUBSTRING("River Information", '([0-9]{1,4})' ) AS "river_length" FROM "view_river_info" ;
