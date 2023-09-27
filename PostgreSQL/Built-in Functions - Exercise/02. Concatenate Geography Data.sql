2280e13c426d3f3373fcc16f49e58fe816b8cfd4
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-27 10:59:40 +0300

    Update 02. Concatenate Geography Data.sql

--- PostgreSQL/Built-in Functions - Exercise/02. Concatenate Geography Data.sql
+++ PostgreSQL/Built-in Functions - Exercise/02. Concatenate Geography Data.sql
@@ -1,10 +1,8 @@
 CREATE VIEW "view_continents_countries_currencies_details" AS
-	SELECT CONCAT("continents"."continent_name", ': ', "continents"."continent_code" ) AS "Continent Details", 
+	SELECT CONCAT(c.continent_name, ': ', c.continent_code ) AS "Continent Details", 
 	CONCAT_WS(' - ', "countries"."country_name", "countries"."capital", "countries"."area_in_sq_km", 'km2')  AS "Country Information",
 	CONCAT("currencies"."description", ' (', "currencies"."currency_code", ')' ) AS "Currencies"
-	FROM continents
-	JOIN countries ON  continents.continent_code = countries.continent_code
+	FROM continents AS c
+	JOIN countries ON  c.continent_code = countries.continent_code
 	JOIN currencies ON currencies.currency_code = countries.currency_code
 	ORDER BY "Country Information" ASC, "Currencies" ASC;
-
-	
