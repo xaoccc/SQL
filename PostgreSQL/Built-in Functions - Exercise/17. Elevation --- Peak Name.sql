69eb0188e44e4343385565bc464b79f105eb16b3
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-27 21:38:02 +0300

    Update 17. Elevation --- Peak Name.sql

--- PostgreSQL/Built-in Functions - Exercise/17. Elevation --- Peak Name.sql
+++ PostgreSQL/Built-in Functions - Exercise/17. Elevation --- Peak Name.sql
@@ -1,2 +1,2 @@
-SELECT CONCAT("elevation", ' ', REPEAT('-', 3), REPEAT('>', 2), ' ', "peak_name" ) AS "Elevation -->> Peak Name" FROM "peaks"
+SELECT CONCAT("elevation", ' ', REPEAT('-', 3), REPEAT('>', 2), ' ', "peak_name" ) AS "Elevation --->> Peak Name" FROM "peaks"
 WHERE elevation >= 4884;
