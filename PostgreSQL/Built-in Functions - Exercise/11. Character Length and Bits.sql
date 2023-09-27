3e53c53951a8f3fba10769f42db2d9b517d5310c
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-27 21:23:26 +0300

    Update 11. Character Length and Bits.sql

--- PostgreSQL/Built-in Functions - Exercise/11. Character Length and Bits.sql
+++ PostgreSQL/Built-in Functions - Exercise/11. Character Length and Bits.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT("mountain_range", ' ', p."peak_name") AS "Mountain Information",
     LENGTH(CONCAT("mountain_range", ' ', p."peak_name")) AS "Characters Length",
     BIT_LENGTH(CONCAT("mountain_range", ' ', p."peak_name")) AS "Bits of a String"
-    FROM "mountains" AS m
-JOIN peaks AS p ON m."id" = p."mountain_id";
+    FROM "mountains" AS m, peaks AS p
+WHERE m."id" = p."mountain_id";
 
 
