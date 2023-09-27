f61b805927807c2aef2d48ee1fab9c4a91d69798
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-27 11:18:49 +0300

    Update 06. Replace A.sql

--- PostgreSQL/Built-in Functions - Exercise/06. Replace A.sql
+++ PostgreSQL/Built-in Functions - Exercise/06. Replace A.sql
@@ -1,4 +1,1 @@
-CREATE VIEW "replace" AS
-	SELECT REPLACE("mountain_range", 'a', '@') AS "replace_a", REPLACE("mountain_range", 'A', '$') AS "replace_A" 	FROM "mountains"; 	
-SELECT * FROM "replace";
-	
+SELECT REPLACE("mountain_range", 'a', '@') AS "replace_a", REPLACE("mountain_range", 'A', '$') AS "replace_A" FROM "mountains"; 	
