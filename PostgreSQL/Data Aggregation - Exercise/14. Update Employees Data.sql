5301d511d5e7747ece97c9ad7d02c15d56e21c3e
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-28 21:30:00 +0300

    Update 14. Update Employees Data.sql

--- PostgreSQL/Data Aggregation - Exercise/14. Update Employees Data.sql
+++ PostgreSQL/Data Aggregation - Exercise/14. Update Employees Data.sql
@@ -1,9 +1,12 @@
 UPDATE "employees"
-SET "job_title" = CONCAT('Senior ', "job_title"), salary = salary + 2500
-WHERE "hire_date" < '2015-01-16';
-
-UPDATE "employees"
-SET "job_title" = CONCAT('Mid-', "job_title"), salary = salary + 1500
-WHERE "hire_date" < '2020-03-04' AND "hire_date" >= '2015-01-16';
-
-SELECT first_name, job_title, salary FROM employees;
+SET 
+  salary = CASE
+    WHEN "hire_date" < '2015-01-16' THEN salary + 2500 
+    WHEN "hire_date" < '2020-03-04' THEN salary + 1500
+    ELSE salary
+  END,
+  job_title = CASE
+    WHEN "hire_date" < '2015-01-16' THEN CONCAT('Senior ', "job_title") 
+    WHEN "hire_date" < '2020-03-04' THEN CONCAT('Mid-', "job_title")
+    ELSE job_title
+  END;
