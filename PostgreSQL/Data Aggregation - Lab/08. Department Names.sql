80c1b2b01b22a996a1f8fca60febb46fb02191d6
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-28 16:08:30 +0300

    Update 08. Department Names.sql

--- PostgreSQL/Data Aggregation - Lab/08. Department Names.sql
+++ PostgreSQL/Data Aggregation - Lab/08. Department Names.sql
@@ -1,8 +1,8 @@
 SELECT "id", first_name, last_name,	salary,	department_id,	
-  CASE
-    WHEN department_id = 1 THEN 'Management'
-    WHEN department_id = 2 THEN 'Kitchen Staff'
-    WHEN department_id = 3 THEN 'Service Staff'
+  CASE department_id
+    WHEN 1 THEN 'Management'
+    WHEN 2 THEN 'Kitchen Staff'
+    WHEN 3 THEN 'Service Staff'
     ELSE 'Other'
   END
   AS department_name FROM employees
