2035c48fe85e3ee3b0f91756602a9bf274fe532c
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-28 21:59:42 +0300

    Update 19. Foreign Key.sql

--- PostgreSQL/Data Aggregation - Exercise/19. Foreign Key.sql
+++ PostgreSQL/Data Aggregation - Exercise/19. Foreign Key.sql
@@ -2,10 +2,8 @@ CREATE TABLE IF NOT EXISTS "employees_projects"
 ("id" SERIAL PRIMARY KEY, 
  "employee_id" INTEGER, 
  "project_id" INTEGER,
- CONSTRAINT fk_employee
  FOREIGN KEY (employee_id)
  REFERENCES employees(id),
- CONSTRAINT fk_project 
  FOREIGN KEY (project_id)
  REFERENCES projects(id)
 );
