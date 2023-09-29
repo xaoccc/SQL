2629908f75805b47ee8772964586def991a25f69
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-09-29 16:44:59 +0300

    Update 2. Trip Organization.sql

--- PostgreSQL/Table Relations - Lab/2. Trip Organization.sql
+++ PostgreSQL/Table Relations - Lab/2. Trip Organization.sql
@@ -1,3 +1,3 @@
-SELECT vehicles.driver_id, vehicle_type, CONCAT(campers.first_name, ' ', campers.last_name) AS driver_name
-FROM vehicles
-JOIN campers ON vehicles.driver_id = campers.id;
+SELECT v.driver_id, vehicle_type, CONCAT(c.first_name, ' ', c.last_name) AS driver_name
+FROM vehicles AS v
+JOIN campers as c ON v.driver_id = c.id;
