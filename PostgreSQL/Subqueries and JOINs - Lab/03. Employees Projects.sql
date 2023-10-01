fc54f0be768b6a921d31683b5a35ce04738d5c19
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-10-01 15:30:34 +0300

    Update 03. Employees Projects.sql

--- PostgreSQL/Subqueries and JOINs - Lab/03. Employees Projects.sql
+++ PostgreSQL/Subqueries and JOINs - Lab/03. Employees Projects.sql
@@ -1,7 +1,7 @@
 SELECT e.employee_id, CONCAT(first_name, ' ', last_name) AS full_name, ep.project_id, p.name AS project_name FROM  employees AS e
 	JOIN employees_projects AS ep USING (employee_id)
 		JOIN projects AS p USING (project_id)
-WHERE p.project_id = 1;
+WHERE project_id = 1;
 
 
 
