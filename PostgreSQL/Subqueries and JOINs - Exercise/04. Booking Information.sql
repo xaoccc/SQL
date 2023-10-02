afaeaca8c01dbe03b8128a1e6f3c1fa46d191b72
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-02 12:02:14 +0300

    Update 04. Booking Information.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/04. Booking Information.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/04. Booking Information.sql
@@ -1,4 +1,9 @@
-SELECT "bookings"."booking_id" AS "Booking ID", "apartments"."name" AS "Apartment Owner",  "apartments"."apartment_id"  AS "Apartment ID", CONCAT("customers"."first_name", ' ', "customers"."last_name") AS "Customer Name" FROM "bookings"
-FULL JOIN "apartments" ON "bookings"."booking_id" = "apartments"."booking_id"
-FULL JOIN "customers" ON "customers"."customer_id" = "bookings"."customer_id"
-ORDER BY "Booking ID", "Apartment Owner", "Customer Name";
+SELECT 
+  b.booking_id AS "Booking ID", 
+  a.name AS "Apartment Owner",
+  a.apartment_id AS "Apartment ID",
+  CONCAT(c.first_name, ' ', c.last_name) AS "Customer Name"
+  FROM bookings AS b
+  FULL JOIN apartments AS a USING (booking_id)
+  FULL JOIN customers AS c USING (customer_id)
+  ORDER BY "Booking ID" ASC, "Apartment Owner" ASC, "Customer Name" ASC;
