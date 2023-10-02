d4f389940c11f64fc470afa9560739389f5fed2a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-02 16:33:46 +0300

    Update 17. Monasteries by Continents and Countries.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/17. Monasteries by Continents and Countries.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/17. Monasteries by Continents and Countries.sql
@@ -3,18 +3,17 @@ UPDATE "countries"
   WHERE country_name = 'Myanmar' ;
 	
 INSERT INTO "monasteries" ("monastery_name", "country_code")
-VALUES ('Hanga Abbey', 'TZ'),
-  ('Myin-Tin-Daik', 'MM')  ;
+VALUES ('Hanga Abbey', 'TZ');
   
 SELECT 
   con.continent_name AS "Continent Name",
   cou.country_name AS "Country Name",
   COUNT(m.monastery_name) AS "Monasteries Count"
-  FROM continents AS con
-  LEFT JOIN countries AS cou USING (continent_code)
+  FROM countries AS cou
+  LEFT JOIN continents AS con USING (continent_code)
   LEFT JOIN monasteries AS m USING (country_code)
   WHERE "three_rivers" = FALSE OR "three_rivers" IS NULL
-  GROUP BY continent_name, country_name
+  GROUP BY country_name, continent_name
   ORDER BY "Monasteries Count" DESC, "Country Name" ASC;
 
 
