2af9bbcad537f15acdd0aed90c45b2824aa1f033
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-02 11:02:51 +0300

    Update 01. Booked for Nights.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/01. Booked for Nights.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/01. Booked for Nights.sql
@@ -5,4 +5,5 @@ SELECT
   JOIN apartments AS a USING (booking_id)
   ORDER BY a.apartment_id	ASC;
 
-#Как знаем да join-ваме по booking_id, a не по apartment_id?
+Как знаем да join-ваме по booking_id, a не по apartment_id?
+Имаме общо 4 варианта за join: 1. а.apartment_id = b.booking_id, 2. а.booking_id = b.apartment_id, 3. USING (booking_id), 4. USING (apartment_id). Какво става, ако бяха 6 или 8? Да тестваме всяка комбинация ли?
