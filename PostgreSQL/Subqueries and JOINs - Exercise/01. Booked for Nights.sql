a7fd347a125b6a42e335faae5325ae2f00e029df
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-02 10:59:13 +0300

    Update 01. Booked for Nights.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/01. Booked for Nights.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/01. Booked for Nights.sql
@@ -1,3 +1,8 @@
-SELECT CONCAT("address", ' ', "address_2") AS "Apartment Address", "bookings"."booked_for" AS "Nights"  FROM "apartments"
-JOIN "bookings" ON "bookings"."booking_id" = "apartments"."booking_id"
-ORDER BY "apartments"."apartment_id";
+SELECT 
+  CONCAT(a.address, ' ', a.address_2) AS "Apartment Address", 
+  b.booked_for AS "Nights" 
+  FROM bookings AS b
+  JOIN apartments AS a USING (booking_id)
+  ORDER BY a.apartment_id	ASC;
+
+#Как знаем да join-ваме по booking_id, a не по apartment_id?
