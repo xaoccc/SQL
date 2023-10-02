1f0f747e4b479a32eee7cffce2ed918d0b5c39c8
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-02 14:52:04 +0300

    Update 08. Hahn`s Bookings.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/08. Hahn`s Bookings.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/08. Hahn`s Bookings.sql
@@ -1,2 +1,7 @@
-SELECT COUNT("last_name") as "count" FROM "customers"
+SELECT COUNT("last_name") FROM "customers"
 WHERE "last_name"='Hahn';
+
+SELECT 
+  COUNT(b.customer_id) FROM bookings AS b
+  JOIN customers AS c USING (customer_id)
+  WHERE c.last_name LIKE '%Hahn%';
