493e6764d52ead2a9fa48f96f2164804be9c92ba
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-02 15:15:34 +0300

    Update 10. Popular Vacation Destination.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/10. Popular Vacation Destination.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/10. Popular Vacation Destination.sql
@@ -1,5 +1,8 @@
-SELECT "country", COUNT("bookings"."booking_id") AS booking_count FROM "apartments"
-JOIN "bookings" USING("apartment_id")
-WHERE booked_at > '2021-05-18 07:52:09.904+03' and booked_at < '2021-09-17 19:48:02.147+03'
-GROUP BY "apartments"."country"
-ORDER BY booking_count DESC;
+SELECT 
+  a.country, COUNT(b.booking_id) as booking_count FROM bookings AS b
+  JOIN apartments AS a USING (apartment_id)
+  WHERE b.booked_at > '2021-05-18 07:52:09.904+03' AND b.booked_at < '2021-09-17 19:48:02.147+03'
+  GROUP BY a.country
+  ORDER BY booking_count DESC;
+
+Тук отново тряба да налучкваме. Първо не се знае country от коя таблица се взима - apartments или customers. След това отново не е ясно по какво джойнваме. Общо комбинациите са 8. 4 за apartments и 4 за customers.
