fb4e2d749f976a468aaf9f2affc5f38bb67f9429
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-03 10:48:54 +0300

    Update 20. The Highest Peak in Each Country.sql

--- PostgreSQL/Subqueries and JOINs - Exercise/20. The Highest Peak in Each Country.sql
+++ PostgreSQL/Subqueries and JOINs - Exercise/20. The Highest Peak in Each Country.sql
@@ -17,3 +17,22 @@ FROM (
 ) AS ranked_peaks
 WHERE rank_within_country = 1
 ORDER BY country_name;
+
+
+-- SELECT "Country", "Highest Peak Name", "Highest Peak Elevation", "Mountain"
+-- FROM (
+--     SELECT 
+--         c.country_name AS "Country",
+--         COALESCE(p.peak_name, '(no highest peak)') AS "Highest Peak Name" ,
+--         COALESCE(p.elevation, 0) AS "Highest Peak Elevation",
+--         COALESCE(m.mountain_range, '(no mountain)') AS "Mountain",
+--         ROW_NUMBER() OVER (PARTITION BY c.country_name ORDER BY p.elevation DESC) AS rank_within_country
+
+--     FROM  countries AS c
+--         LEFT JOIN mountains_countries AS mC ON  c.country_code = mC.country_code
+--         LEFT JOIN mountains AS m ON m.id = mC.mountain_id
+--         LEFT JOIN peaks AS p ON m.id = p.mountain_id
+
+-- ) AS subquery
+-- WHERE rank_within_country = 1
+-- ORDER BY "Country" ASC, "Highest Peak Elevation" DESC;
