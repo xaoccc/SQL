09300865108d98c3484667292cf369df01edda37
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-04 10:51:21 +0300

    Update 01. Count Employees by Town.sql

--- PostgreSQL/Database Programmability - Lab/01. Count Employees by Town.sql
+++ PostgreSQL/Database Programmability - Lab/01. Count Employees by Town.sql
@@ -3,7 +3,8 @@ RETURNS INT AS
 $$
   DECLARE people_count INT;
   BEGIN
-    SELECT COUNT(t.town_id) INTO people_count FROM  employees AS e
+    SELECT COUNT(e.employee_id) INTO people_count 
+    FROM  employees AS e
     JOIN addresses AS a USING (address_id)
     JOIN towns AS t USING (town_id)
     WHERE t.name = town_name;
@@ -11,3 +12,18 @@ $$
   END;
 $$
 LANGUAGE plpgsql;
+
+-- Another solution (without using people_count):
+-- CREATE FUNCTION fn_count_employees_by_town(town_name VARCHAR(20))
+-- RETURNS INT AS
+-- $$   
+--   BEGIN
+--     RETURN (
+--     SELECT COUNT(e.employee_id) 
+--     FROM  employees AS e
+--     JOIN addresses AS a USING (address_id)
+--     JOIN towns AS t USING (town_id)
+--     WHERE t.name = town_name);
+--   END;
+-- $$
+-- LANGUAGE plpgsql;
