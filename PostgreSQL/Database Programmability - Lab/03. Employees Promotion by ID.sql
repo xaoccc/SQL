638d718a7b73b88af3caf69d810a820d6d4c8a31
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-04 12:43:51 +0300

    Update 03. Employees Promotion by ID.sql

--- PostgreSQL/Database Programmability - Lab/03. Employees Promotion by ID.sql
+++ PostgreSQL/Database Programmability - Lab/03. Employees Promotion by ID.sql
@@ -12,3 +12,26 @@ $$
   END
 $$
 LANGUAGE plpgsql;
+
+-- With if-elif statement
+-- CREATE OR REPLACE PROCEDURE sp_increase_salary_by_id(e_id INT) 
+-- AS
+-- $$
+--   BEGIN
+    
+--     IF (SELECT salary FROM employees WHERE employee_id = e_id) IS NULL THEN
+--       RETURN;
+      
+--     ELSE
+--       UPDATE employees
+--       SET salary = salary * 1.05
+--         WHERE employee_id = (
+--         SELECT employee_id 
+--         FROM employees AS e 
+--         JOIN departments AS d USING (department_id)
+--         WHERE employee_id = e_id);
+--     END IF;
+--     COMMIT;
+--   END
+-- $$
+-- LANGUAGE plpgsql;
