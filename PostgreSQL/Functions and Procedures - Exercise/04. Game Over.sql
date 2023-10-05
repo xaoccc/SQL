84c35aebd2c2f7c58814389bdec24b4e1a05c93a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-05 12:59:58 +0300

    Update 04. Game Over.sql

--- PostgreSQL/Functions and Procedures - Exercise/04. Game Over.sql
+++ PostgreSQL/Functions and Procedures - Exercise/04. Game Over.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE FUNCTION fn_is_game_over(is_game_over BOOLEAN)
 RETURNS TABLE(name VARCHAR, game_type_id INT, is_finished BOOL) AS 
 $$
-DECLARE result INT;
 BEGIN 
   FOR name, game_type_id, is_finished IN SELECT g.name, g.game_type_id, g.is_finished FROM games AS g 
     WHERE g.is_finished = is_game_over
