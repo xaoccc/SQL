5bc1d56f51f57bb1f0d86801437795cb1cae8e55
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-06 15:44:06 +0300

    Create 12. Log Accounts Trigger.sql

---
+++ PostgreSQL/Functions and Procedures - Exercise/12. Log Accounts Trigger.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS logs (
+ id SERIAL PRIMARY KEY, 
+ account_id INT, 
+ old_sum NUMERIC, 
+ new_sum NUMERIC);
+
+ CREATE FUNCTION trigger_fn_insert_new_entry_into_logs()
+ RETURNS TRIGGER AS 
+ $$
+ BEGIN
+
+  RETURN NEW;
+
+ END;
+ $$
+
+ CREATE TRIGGER tr_account_balance_change
+ AFTER UPDATE OF balance ON accounts
+ FOR EACH ROW
+ WHEN (NEW.balance <> OLD.balance)
+ EXECUTE PROCEDURE trigger_fn_insert_new_entry_into_logs();
