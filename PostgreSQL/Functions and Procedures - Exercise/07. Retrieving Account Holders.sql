2f87a9d7745ad529ab1df3834685553593d0c0df
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-06 14:20:10 +0300

    Update 07. Retrieving Account Holders.sql

--- PostgreSQL/Functions and Procedures - Exercise/07. Retrieving Account Holders.sql
+++ PostgreSQL/Functions and Procedures - Exercise/07. Retrieving Account Holders.sql
@@ -1,7 +1,8 @@
 -- correction 1. - add holder_info RECORD;
 -- correction 2. - add OR REPLACE
 -- correction 3. - remove variables name and total_balance and statements INTO name,  INTO total_balance
--- correction 2. - add OR REPLACE
+-- correction 4. - add OR REPLACE
+-- correction 5. - replace RAISE NOTICE 'TEST' with  RAISE NOTICE '% - %', holder_info.full_name, holder_info.balance_sum;
 
 CREATE OR REPLACE PROCEDURE sp_retrieving_holders_with_balance_higher_than(searched_balance DECIMAL)
 AS
@@ -16,7 +17,7 @@ BEGIN
     HAVING SUM(balance) > searched_balance 
     
   LOOP
-    RAISE NOTICE 'TEST';
+    RAISE NOTICE '% - %', holder_info.full_name, holder_info.balance_sum;
   END LOOP;
   
 END; 
