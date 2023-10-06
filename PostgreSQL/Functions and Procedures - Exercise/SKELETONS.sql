d57a126e8550bbe8b7a0cb627f87eedbcde097ed
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-06 15:55:17 +0300

    Update SKELETONS.sql

--- PostgreSQL/Functions and Procedures - Exercise/SKELETONS.sql
+++ PostgreSQL/Functions and Procedures - Exercise/SKELETONS.sql
@@ -10,11 +10,18 @@ LANGUAGE plpgsql;
 
 
 -- PROCEDURE
-CREATE OR REPLACE PROCEDURE fn_function_name(parameter 1 value_type, parameter 2 value_type, ...)
-RETURNS value_type AS 
+CREATE OR REPLACE PROCEDURE sp_procedure_name(parameter 1 value_type, parameter 2 value_type, ...)
+AS 
 $$
 BEGIN
-
+  RETURN OLD(when delete)/NEW(when create/update)
 END ;
 $$
 LANGUAGE plpgsql;
+
+
+-- TRIGGER
+ CREATE OR REPLACE TRIGGER tr_trigger_name
+ AFTER CREATE/UPDATE/DELETE OF column_name ON table_name
+ FOR EACH ROW/
+ EXECUTE PROCEDURE sp_procedure_name(parameter 1 value_type, parameter 2 value_type, ...);
