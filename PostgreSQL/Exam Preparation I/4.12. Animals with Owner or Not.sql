bccf573e70006b94b9ec10e443454ec2f5f97bc8
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-10-07 19:46:47 +0300

    Create 4.12. Animals with Owner or Not.sql

---
+++ PostgreSQL/Exam Preparation I/4.12. Animals with Owner or Not.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not( 
+	IN animal_name VARCHAR(30), 
+	OUT o_name VARCHAR(30)
+)
+AS
+$$
+
+BEGIN
+
+	SELECT
+		o.name 
+	FROM owners AS o
+	JOIN animals AS a ON a.owner_id=o.id
+	WHERE a.name = animal_name
+	INTO o_name;
+	IF o_name IS NULL THEN
+		o_name := 'For adoption';
+	END IF;
+	RETURN;
+
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CALL sp_animals_with_owners_or_not('Hippo')
