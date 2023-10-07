edca0d6a961bf78137a0117b05f2de9bd58d844c
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-10-07 16:14:16 +0300

    AJKASKJDJ

--- PostgreSQL/Exam Preparation I/3.7. Owners and Their Animals.sql
+++ PostgreSQL/Exam Preparation I/3.7. Owners and Their Animals.sql
@@ -3,4 +3,6 @@ SELECT
 	COUNT(a.name) AS count_of_animals
 FROM owners AS o
 JOIN animals AS a ON o.id = a.owner_id
-GROUP BY o.name;
+GROUP BY o.name
+ORDER BY count_of_animals DESC, owner ASC
+LIMIT 5;
