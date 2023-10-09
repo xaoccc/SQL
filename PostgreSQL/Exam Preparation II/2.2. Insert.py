879cd6c846d50d56719a1273c6056fc077c135da
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-09 13:24:29 +0300

    Update 2.2. Insert.py

--- PostgreSQL/Exam Preparation II/2.2. Insert.py
+++ PostgreSQL/Exam Preparation II/2.2. Insert.py
@@ -1,6 +1,6 @@
 INSERT INTO clients (full_name, phone_number)
 SELECT 
   CONCAT(first_name, ' ', last_name) AS full_name, 
-  CONCAT('(088) 9999', (d.id*2)::VARCHAR) AS phone_number 
+  CONCAT('(088) 9999', (d.id*2)::CHAR(2)) AS phone_number 
 FROM drivers AS d 
 WHERE d.id BETWEEN 10 AND 20;
