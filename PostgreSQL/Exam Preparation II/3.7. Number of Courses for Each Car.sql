9178ca8113d4d5f06cd039ce5ce4a98a004440e6
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-09 15:25:41 +0300

    Create 3.7. Number of Courses for Each Car.sql

---
+++ PostgreSQL/Exam Preparation II/3.7. Number of Courses for Each Car.sql
@@ -0,0 +1,14 @@
+SELECT 
+  car_id, 
+  make, 
+  mileage, 
+  COALESCE(COUNT(c.id), 0) AS count_of_courses, 
+  CASE WHEN COUNT(c.id) = 0 THEN NULL 
+    ELSE ROUND(AVG(c.bill), 2) 
+    END 
+    AS average_bill 
+FROM cars
+LEFT JOIN courses AS c ON cars.id = c.car_id
+GROUP BY car_id, make, mileage
+HAVING COALESCE(COUNT(c.id), 0) <> 2
+ORDER BY count_of_courses DESC, c.car_id;
