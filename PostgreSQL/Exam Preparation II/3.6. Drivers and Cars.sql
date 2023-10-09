f0fa2c03febc183d0b610a8feacffed2dbe57680
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-09 14:31:48 +0300

    Create 3.6. Drivers and Cars.sql

---
+++ PostgreSQL/Exam Preparation II/3.6. Drivers and Cars.sql
@@ -0,0 +1,5 @@
+SELECT d.first_name, d.last_name, c.make, c.model, c.mileage FROM cars AS c
+JOIN cars_drivers AS cd ON c.id = cd.car_id
+JOIN drivers AS d ON d.id = cd.driver_id
+WHERE c.mileage IS NOT NULL
+ORDER BY mileage DESC, first_name ASC;
