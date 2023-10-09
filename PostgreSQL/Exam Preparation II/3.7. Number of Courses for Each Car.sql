7aff786c7ec85bd424b86162a914549547bdd2a0
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-09 15:51:02 +0300

    Update 3.7. Number of Courses for Each Car.sql

--- PostgreSQL/Exam Preparation II/3.7. Number of Courses for Each Car.sql
+++ PostgreSQL/Exam Preparation II/3.7. Number of Courses for Each Car.sql
@@ -1,7 +1,7 @@
 SELECT 
-  car_id, 
-  make, 
-  mileage, 
+  cars.id, 
+  cars.make, 
+  cars.mileage, 
   COALESCE(COUNT(c.id), 0) AS count_of_courses, 
   CASE WHEN COUNT(c.id) = 0 THEN NULL 
     ELSE ROUND(AVG(c.bill), 2) 
@@ -9,8 +9,6 @@ SELECT
     AS average_bill 
 FROM cars
 LEFT JOIN courses AS c ON cars.id = c.car_id
-GROUP BY car_id, make, mileage
-HAVING COALESCE(COUNT(c.id), 0) <> 2
-ORDER BY count_of_courses DESC, c.car_id;
-
--- защо не се появяват id-тата на колите???
+GROUP BY cars.id, make, mileage
+HAVING COUNT(cars.id) <> 2
+ORDER BY count_of_courses DESC, cars.id;
