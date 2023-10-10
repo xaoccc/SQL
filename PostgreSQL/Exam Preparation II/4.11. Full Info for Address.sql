ef26786aefda62decb6efd1874ba87ce91cafa2e
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-10 15:34:13 +0300

    Update 4.11. Full Info for Address.sql

--- PostgreSQL/Exam Preparation II/4.11. Full Info for Address.sql
+++ PostgreSQL/Exam Preparation II/4.11. Full Info for Address.sql
@@ -14,23 +14,27 @@ CREATE OR REPLACE PROCEDURE sp_courses_by_address(
 AS 
 $$
   BEGIN
+    TRUNCATE TABLE search_results;
     INSERT INTO search_results (address_name, full_name, level_of_bill, make, condition, category_name)
-    SELECT a.name AS address_name, c.full_name, 
-      
-      CASE WHEN co.bill <= 20 THEN 'Low'
-      WHEN co.bill > 20 AND co.bill <= 30 THEN 'Medium'
-      ELSE 'High'
+    SELECT 
+      a.name AS address_name, 
+      c.full_name,       
+      CASE 
+        WHEN co.bill <= 20 THEN 'Low'
+        WHEN co.bill > 20 AND co.bill <= 30 THEN 'Medium'
+        ELSE 'High'
       END
-       AS level_of_bill, 
-      cars.make, cars.condition, cat.name AS category_name FROM clients AS c
+      AS level_of_bill, 
+      cars.make, 
+      cars.condition, 
+      cat.name AS category_name 
+    FROM clients AS c
     JOIN courses AS co ON c.id = co.client_id
     JOIN addresses AS a ON a.id = co.from_address_id
     JOIN cars ON co.car_id = cars.id 
     JOIN categories AS cat ON cat.id = cars.category_id
     WHERE a.name = address_name
     ORDER BY cars.make, c.full_name;
-
-
   END;
 $$
 LANGUAGE plpgsql;
