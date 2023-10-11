2575689bacf2e51b8f76bbe7498cbbd22e50c4e7
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-11 12:16:50 +0300

    Update 2. 3. Update.sql

--- PostgreSQL/Exam Preparation III/2. 3. Update.sql
+++ PostgreSQL/Exam Preparation III/2. 3. Update.sql
@@ -1,7 +1,7 @@
-UPDATE players_ranges
+UPDATE players_ranges CASCADE
   SET max_players = max_players + 1
   WHERE max_players = 2 AND min_players = 2;
 
-UPDATE board_games
+UPDATE board_games CASCADE
   SET name = CONCAT(name, ' V2')
   WHERE release_year >= 2020;
