c4bf59897907f1f2978b217df95b634ea2036edc
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-13 11:49:42 +0300

    Update 1693. Daily Leads and Partners.sql

--- leetcode/1693. Daily Leads and Partners.sql
+++ leetcode/1693. Daily Leads and Partners.sql
@@ -1,4 +1,3 @@
-SELECT date_id, make_name, COUNT(DISTINCT lead_id) as unique_leads, COUNT(DISTINCT partner_id) as unique_partners
-FROM DailySales
-GROUP BY date_id, make_name
-ORDER BY date_id, make_name;
+SELECT date_id, make_name, COUNT(DISTINCT(lead_id)) AS unique_leads, COUNT(DISTINCT(partner_id)) AS unique_partners FROM DailySales 
+GROUP BY date_id, make_name;
+
